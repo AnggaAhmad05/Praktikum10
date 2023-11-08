6b7cf40d55678398b08363ce380272f4c7d4d7fd
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-08 12:02:21 +0700

    langkah11

--- Bioskop05.java
+++ Bioskop05.java
@@ -27,11 +27,12 @@ public class Bioskop05 {
         }
         System.out.println("Penonton pada garis ke-3: ");
 
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
-            
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+        
         }
 }
 
     
-}
+
